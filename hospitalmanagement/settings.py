27e318b24e65f945059c25a03a776326b349fe53
saran KS <125279198+x22102817Raj@users.noreply.github.com>
2023-04-17 12:42:46 +0530

    static admin

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -137,8 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
