ddcad4b47f845f78dc88f168e75c7bfb23661302
saran KS <125279198+x22102817Raj@users.noreply.github.com>
2023-04-17 12:16:05 +0530

    static files

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -90,23 +90,14 @@ if 'RDS_HOSTNAME' in os.environ:
         }
     }
 else:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #     }
-    # }
-    
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'student_management',
-            'USER':'root',
-            'PASSWORD':'liveandletlive',
-            'HOST':'localhost',
-            'PORT':'3306'
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+    
+  
 
 
 # Password validation
@@ -145,11 +136,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS=[STATIC_DIR,]
+
+# MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+
+
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS=[STATIC_DIR,]
-
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
